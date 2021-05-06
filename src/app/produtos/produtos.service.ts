7e6a697e62e84a5bc61682dc4ae7625ef0863c3a
webersoncarneiro <webersoncoimbra2014@gmail.com>
2021-05-05 22:40:48 -0300

    refactoring the look of the application

--- src/app/produtos/produtos.service.ts
+++ src/app/produtos/produtos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Produto } from "./produto";
 import { Observable } from "rxjs";
+import { CATCH_ERROR_VAR } from "@angular/compiler/src/output/output_ast";
 
 @Injectable()
 export class ProdutoService {
@@ -9,9 +10,10 @@ export class ProdutoService {
    constructor(private http: HttpClient) { }
 
     protected UrlServiceV1: string = "http://localhost:3000/";
-
+      
     obterProdutos() : Observable<Produto[]> {
        return this.http
        .get<Produto[]>(this.UrlServiceV1 + "produtos");
+       
     }
 }
